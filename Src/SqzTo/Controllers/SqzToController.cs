17a5d2ce531cae48ab0802722e318e113b05c75e
bratistolf <bratistolf@gmail.com>
2020-07-27 12:11:36 +0500

    Initial project and file system setup

---
+++ Src/SqzTo/Controllers/SqzToController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace SqzTo.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SqzToController : Controller
+    {
+        [HttpPost, Route("/")]
+        public async Task<IActionResult> GetOrCreateFrom([FromBody] string url)
+        {
+            //FUCKOFF: The Official Site of a Symbolic Behavior Model Modification Suggestion - https://natribu.org/en/
+            return Json("TOSSBMMS");
+        }
+
+        [HttpGet, Route("/{route}")]
+        public async Task<IActionResult> NavigateTo([FromRoute] string route)
+        {
+            return Json("https://natribu.org/en/");
+        }
+    }
+}
