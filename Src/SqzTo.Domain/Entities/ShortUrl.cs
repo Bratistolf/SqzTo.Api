6e806dffbd8fb6a58ac758d1cf160fa5a3a29380
bratistolf <bratistolf@gmail.com>
2020-07-27 14:27:45 +0500

    Basic project layout

---
+++ Src/SqzTo.Domain/Entities/ShortUrl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SqzTo.Domain.Entities
+{
+    public class ShortUrl
+    {
+        public int ShortUrlId { get; set; }
+        public string Route { get; set; }
+        public string OriginalUrl { get; set; }
+        public int Clicks { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
