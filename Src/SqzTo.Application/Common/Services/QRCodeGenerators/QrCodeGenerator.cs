1741b67589b7c7782f2fc4dbe28f5d39073d8764
bratistolf <bratistolf@gmail.com>
2020-08-07 20:45:25 +0500

    - Added throw new NotImplemented exception

--- Src/SqzTo.Application/Common/Services/QRCodeGenerators/QrCodeGenerator.cs
+++ Src/SqzTo.Application/Common/Services/QRCodeGenerators/QrCodeGenerator.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using SqzTo.Application.Common.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SqzTo.Application.Common.Services.QRCodeGenerators
 {
-    class QrCodeGenerator
+    public class QrCodeGenerator : IQrCodeGenerator
     {
+        public string GenerateQr(string value)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
